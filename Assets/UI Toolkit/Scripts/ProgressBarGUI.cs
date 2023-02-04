09d1883e91412a1bc1d47139b9f4138e938db555
Víctor I. Castrillo Muñoz <victor.castrillo.99@gmail.com>
2023-02-03 19:09:43 -0600

    add stat bar

---
+++ Assets/UI Toolkit/Scripts/ProgressBarGUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace HudElements
+{
+    public class ProgressBarGUI : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<ProgressBarGUI, UxmlTraits> { }
+    }
+}
