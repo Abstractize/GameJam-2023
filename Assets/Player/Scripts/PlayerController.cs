32f1940a00cd555863a2e50e9cac8e0c3982b7c3
Víctor I. Castrillo Muñoz <victor.castrillo.99@gmail.com>
2023-02-04 14:29:24 -0600

    add new hud

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -11,17 +11,22 @@ namespace Player
         [SerializeField] public NetworkPlayer Player { get; set; }
         [HideInInspector] public GameAction Action { get; set; } = new();
         [HideInInspector] public Wallet Wallet { get; set; } = new();
+        [HideInInspector] public PlayerStats Stats { get; set; } = new();
         [SerializeField]
         [Range(10, 100)]
         private float _waitTime = 15;
         [SerializeField] private bool _isGeneratingMoney = true;
+        [SerializeField] private bool _isEnabledStats = true;
 
         private const int EARNING = 10;
         public int Level { get; private set; } = 1;
 
+        private const int DECAY = 1;
+
         private void Start()
         {
             StartCoroutine(nameof(GenerateMoney));
+            StartCoroutine(nameof(EnableStats));
         }
 
         public void OnFire(InputAction.CallbackContext context)
@@ -44,6 +49,20 @@ namespace Player
                 // Trigger Wallet Animation
 
                 Debug.Log($"You have {Wallet.Money} money");
+                yield return new WaitForSeconds(_waitTime);
+            }
+        }
+
+        private IEnumerator EnableStats()
+        {
+            while (_isEnabledStats)
+            {
+                Stats.hunger -= DECAY;
+                Stats.sleep -= DECAY;
+                Stats.fun -= DECAY;
+                Stats.hygiene -= DECAY;
+
+                Debug.Log($"Stats lower");
                 yield return new WaitForSeconds(_waitTime);
             }
         }
