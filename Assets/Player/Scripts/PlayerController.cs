82b774a114a016805d0de0e05e07129527d66220
Gabriel <gababarca@hotmail.com>
2023-02-04 10:21:38 -0600

    Add Wallet System and Money Generation

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -1,8 +1,6 @@
+using System.Collections;
 using Data;
-using Fusion;
-using Kiosk;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.InputSystem;
 using static Player.PlayerInputs;
 
@@ -12,6 +10,15 @@ namespace Player
     {
         [SerializeField] public NetworkPlayer Player { get; set; }
         [HideInInspector] public GameAction Action { get; set; }
+        [HideInInspector] public Wallet Wallet { get; set; }
+
+        private const int EARNING = 10;
+        public int Level { get; private set; } = 1;
+
+        private void Start()
+        {
+            StartCoroutine(nameof(GenerateMoney));
+        }
 
         public void OnFire(InputAction.CallbackContext context)
             => Action?.Callback.Invoke(Player);
@@ -24,5 +31,11 @@ namespace Player
         public void OnMove(InputAction.CallbackContext context)
             => (Player.MovementVector = context.ReadValue<Vector2>()).Normalize();
 
+        private IEnumerator GenerateMoney()
+        {
+            Wallet.Money += Level * EARNING;
+            // Wallet Animation
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
